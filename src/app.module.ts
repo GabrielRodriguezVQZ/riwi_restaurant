da2f68ad545026180fad6f98c58a531929b66072
Gabriel Rodriguez <gabrielrodriguezvqz@gmail.com>
2026-09-11 20:22:45 -0500

    integration of the initial settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { Module } from '@nestjs/common';
 import { createObserveModule } from '@nestjs/observe';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { EnvConfig, envValidationSchema } from './config/index.js';
 
 export const { ObserveModule, ObserveInstrument } = createObserveModule();
 
@@ -9,13 +11,21 @@ export const { ObserveModule, ObserveInstrument } = createObserveModule();
   imports: [
     // Distributed tracing, auto-correlated logs, request/job metrics, error
     // telemetry, alarms, and more — out of the box. Sign up at https://observe.nestjs.com
-    ObserveModule.forRoot({
-      appKey: 'YOUR_APP_KEY',
-      appSecret: 'YOUR_APP_SECRET',
-      serviceId: 'riwi_restaurant',
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [EnvConfig],
+      validationSchema: envValidationSchema
+    }),
+    ObserveModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (observeConfig: ConfigService) => ({
+        ...observeConfig.getOrThrow('observe')
+      })
+
     }),
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
